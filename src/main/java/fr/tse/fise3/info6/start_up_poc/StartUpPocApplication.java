978c5cb36751b0dd695ce19aa553eeecbf2284f8
Sami Aymane Elouafkaoui <56339835+samiaymane@users.noreply.github.com>
2020-11-25 10:32:11 +0100

    Initial commit

---
+++ src/main/java/fr/tse/fise3/info6/start_up_poc/StartUpPocApplication.java
@@ -0,0 +1,13 @@
+package fr.tse.fise3.info6.start_up_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tse/fise3/info6/start_up_poc/StartUpPocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tse.fise3.info6.start_up_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
