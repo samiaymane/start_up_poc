dfa3e73d39181eeda4b8f2dc77f0017b33f50d5a
Sami Aymane Elouafkaoui <56339835+samiaymane@users.noreply.github.com>
2021-02-08 15:12:05 +0100

    Unit tests controllers

--- src/main/java/fr/tse/fise3/info6/start_up_poc/security/CorsFilter.java
+++ src/main/java/fr/tse/fise3/info6/start_up_poc/security/CorsFilter.java
@@ -1,6 +1,5 @@
 package fr.tse.fise3.info6.start_up_poc.security;
 
-import javax.servlet.http.Cookie;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -9,7 +8,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -26,19 +24,11 @@ public class CorsFilter implements Filter{
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
 
-        javax.servlet.http.Cookie[] allCookies = request.getCookies();
-        if (allCookies != null) {
-            Cookie session =
-                    Arrays.stream(allCookies).filter(x -> x.getName().equals("JSESSIONID"))
-                            .findFirst().orElse(null);
-
-            if (session != null) {
-                session.setHttpOnly(false);
-                session.setSecure(false);
-                response.addCookie(session);
-            }
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
         }
-        chain.doFilter(request, response);
 	}
 
 }
